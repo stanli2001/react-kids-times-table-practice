8827365268b389474c36455f0fcc32885a4f589c
list <Stan.Li@tomtom.com>
2021-09-06 08:11:00 +1000

    add text to award pic

--- src/times-table-practice/components/Popup.js
+++ src/times-table-practice/components/Popup.js
@@ -1,12 +1,12 @@
 import React from "react";
-import './Popup.css'
+import "./Popup.css";
 import logo from "../pictures/Koala.jpg";
 
 function Popup(props) {
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
-        <img className="img" alt="award picture"  src={logo} />        
+        <img className="img" alt="award picture" src={logo} />
         {props.children}
       </div>
     </div>
